1a68357456335da930f7ad0ca672bb3b59b507c7
shankarsharma8089 <126678340+shankarsharma8089@users.noreply.github.com>
2023-06-01 12:32:08 +0530

    edge_detection.py

--- edge_detection.py
+++ edge_detection.py
@@ -6,3 +6,12 @@ cv.imshow('gray',gray)
 lap = cv.Laplacian(gray,cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow('laplacian',lap)
+
+#SOBEL 
+sobelx = cv.Sobel(gray,cv.CV_64F,1,0)
+sobely = cv.Sobel(gray,cv.CV_64F,0,1)
+combined_sobel = cv.bitwise_or(sobelx,sobely)
+
+cv.imshow('sobel x',sobelx)
+cv.imshow('sobel y',sobely)
+cv.imshow('combined sobel',combined_sobel)
